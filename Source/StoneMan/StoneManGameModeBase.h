9825e6d2430d8c44c4e959d39503f3fa1da46a67
dchepurkin <dchepurkin@mail.ru>
2022-06-18 19:13:00 +0300

    СТАРТ

---
+++ Source/StoneMan/StoneManGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StoneManGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STONEMAN_API AStoneManGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
